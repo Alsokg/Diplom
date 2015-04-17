929b3f26d90cffd3d31854e3f4ff5736038fbb13
Alsokg <aloskg@gmail.com>
2015-04-17 10:58:45 +0300

    Update University.cs

--- StudentMonitoringSystem/Models/University.cs
+++ StudentMonitoringSystem/Models/University.cs
@@ -7,8 +7,14 @@ namespace StudentMonitoringSystem.Models
 {
     public class University
     {
+        [HiddenInput (DisplayValue = false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        
+        [Display (Name = "University name")]
+        [Required]
         public String Name { get; set; }
+        [Display (Name = "Address")]
         public String Address { get; set; }
         public virtual ICollection<Faculty> Faculties { get; set; }
     }
