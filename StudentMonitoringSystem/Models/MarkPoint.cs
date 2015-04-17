e50ad6402bcd0f74d7c6a2d9ea817e7b25a7b9cb
Alsokg <aloskg@gmail.com>
2015-04-17 11:00:27 +0300

    Update MarkPoint.cs

--- StudentMonitoringSystem/Models/MarkPoint.cs
+++ StudentMonitoringSystem/Models/MarkPoint.cs
@@ -11,12 +11,15 @@ namespace StudentMonitoringSystem.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Display (Name = "Task name")]
+        [Required]
         public String Name { get; set; }
         [HiddenInput (DisplayName = false)]
         public int? SubjectId { get; set; }
         [Display (Name = "Maximum marks value")]
+        [Required]
         public double maxValue { get; set; }
         [Display (Name = "Deadline")]
+        [Required]
         public Date Date { get; set; }
         public virtual Subject Subject { get; set; }
 
