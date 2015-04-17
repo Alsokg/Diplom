94f65a70f9f278677febfb13dd331e70af27f2dd
Alsokg <aloskg@gmail.com>
2015-04-17 10:49:38 +0300

    Update MarkPoint.cs

--- StudentMonitoringSystem/Models/MarkPoint.cs
+++ StudentMonitoringSystem/Models/MarkPoint.cs
@@ -7,11 +7,16 @@ namespace StudentMonitoringSystem.Models
 {
     public class MarkPoint
     {
+        [HiddenInput (DisplayName = false)]
         public int Id { get; set; }
+        [Display (Name = "Task name")]
         public String Name { get; set; }
+        [HiddenInput (DisplayName = false)]
         public int? SubjectId { get; set; }
+        [Display (Name = "Maximum marks value")]
         public double maxValue { get; set; }
-        public DateTime Date { get; set; }
+        [Display (Name = "Deadline")]
+        public Date Date { get; set; }
         public virtual Subject Subject { get; set; }
 
         public virtual ICollection<Mark> Marks { get; set; }
