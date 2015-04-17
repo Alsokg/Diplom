d1101f9c2c2e0d1ee11b9976dc26cef4332de2f7
Alsokg <aloskg@gmail.com>
2015-04-17 10:59:25 +0300

    Update Student.cs

--- StudentMonitoringSystem/Models/Student.cs
+++ StudentMonitoringSystem/Models/Student.cs
@@ -8,6 +8,7 @@ namespace StudentMonitoringSystem.Models
     public class Student
     {
         [HiddenInput (DisplayValue = false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Dispaly (Name = "FirstName")]
         public String Name { get; set; }
