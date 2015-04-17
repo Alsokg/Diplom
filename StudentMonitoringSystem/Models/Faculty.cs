9f2dae558791d34ea8883d236c6e8c85bbe8ed62
Alsokg <aloskg@gmail.com>
2015-04-17 10:38:25 +0300

    Update Faculty.cs

--- StudentMonitoringSystem/Models/Faculty.cs
+++ StudentMonitoringSystem/Models/Faculty.cs
@@ -7,8 +7,11 @@ namespace StudentMonitoringSystem.Models
 {
     public class Faculty
     {
+        [HiddenInput (DisplayValue = false)]
         public int Id { get; set; }
+        [Display (Name = "Full faculty name")]
         public String Name { get; set; }
+        [HiddenInput (DisplayValue = false)]
         public int? UniversityId { get; set; }
         public virtual University University { get; set; }
         public virtual ICollection<Group> Groups { get; set; }
