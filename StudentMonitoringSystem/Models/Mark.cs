482cb262d82f1ef679228783dfb9d413c4eb0392
Alsokg <aloskg@gmail.com>
2015-04-17 11:01:13 +0300

    Update Mark.cs

--- StudentMonitoringSystem/Models/Mark.cs
+++ StudentMonitoringSystem/Models/Mark.cs
@@ -8,8 +8,10 @@ namespace StudentMonitoringSystem.Models
     public class Mark
     {
         [HiddenInput (DisplayValue = false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Display (Name = "Decemical Value")]
+        [Required]
         public double Value { get; set; }
         [HiddenInput (DisplayValue = false)]
         public int? MarkPointId { get; set; }
