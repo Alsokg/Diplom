27bb5821fb01ca55e64ee0342c9a9b7d92e90ef2
Alsokg <aloskg@gmail.com>
2015-04-17 10:51:17 +0300

    Update Subject.cs

--- StudentMonitoringSystem/Models/Subject.cs
+++ StudentMonitoringSystem/Models/Subject.cs
@@ -7,7 +7,9 @@ namespace StudentMonitoringSystem.Models
 {
     public class Subject
     {
+        [HiddenInput (DisplayValue = false)]
         public int Id { get; set; }
+        [Display (Name = "Course name") ]
         public String Name { get; set; }
         public virtual ICollection<Student> Students { get; set; }
         public virtual ICollection<MarkPoint> Points { get; set; }
