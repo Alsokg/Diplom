661b82fdb5ac5bba14e205250ba2a3ec2c1f8fd1
Alsokg <aloskg@gmail.com>
2015-04-17 10:30:39 +0300

    Update Lector.cs

--- StudentMonitoringSystem/Models/Lector.cs
+++ StudentMonitoringSystem/Models/Lector.cs
@@ -7,9 +7,13 @@ namespace StudentMonitoringSystem.Models
 {
     public class Lector
     {
+        [HiddenInput (DisplayName = false)]
         public int Id { get; set; }
+        [Display (Name = "FirstName")]
         public String Name { get; set; }
+        [Display (Name = "LastName")]
         public String SurName { get; set; }
+        [Display (Name = "Degree")]
         public String Grade { get; set; }
         public virtual ICollection<Subject> Subjects { get; set; }
 
